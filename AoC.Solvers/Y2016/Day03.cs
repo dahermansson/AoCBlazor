5efd54f2b0ec3d75ec3e842b76d102aef4815d00
DaHermansson <daniel.hermansson@gmail.com>
2023-11-04 18:29:25 +0100

    Day3 2016 star1

--- AoC.Solvers/Y2016/Day03.cs
+++ AoC.Solvers/Y2016/Day03.cs
@@ -6,15 +6,21 @@ public class Day03: IDay
 {
     public Day03(string input)
     {
-        Input = input;
+        while(input.Contains("  "))
+            input = input.Replace("  ", " ");
+        Input = InputParsers.GetInputLines(input).Select(t => t.Trim()).ToArray();
     }
     public string Output => throw new NotImplementedException();
 
-    public string Input {get; private set;}
+    public string []Input {get; private set;}
 
     public int Star1()
     {
-        return 0;
+        return Input.Select(t => {
+            var k = t.Split(" ");
+            var d = k.Select(i => int.Parse(i)).OrderBy(p => p);
+            return d.Take(2).Sum() > d.Last();
+            }).Count(t => t);
     }
 
     public int Star2()
