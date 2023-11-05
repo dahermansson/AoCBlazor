8e54a73cb3c68869de7094dc9417bf7239413e4f
DaHermansson <daniel.hermansson@gmail.com>
2023-11-05 12:30:41 +0100

    More fix on day 23

--- AoC.Solvers/Y2015/Day23.cs
+++ AoC.Solvers/Y2015/Day23.cs
@@ -4,51 +4,44 @@ namespace AoC.Solvers.Y2015;
 
 public class Day23 : IDay
 {
-    public Day23(string input)
-    {
-        Input = InputParsers.GetInputLines(input).Select((t, i) =>
-        {
-            var s = t.Split(" ");
-            int off = s.Length == 3 ? int.Parse(s[2], System.Globalization.NumberStyles.AllowLeadingSign) : 0;
-            off = s[0] == "jmp" ? int.Parse(s[1], System.Globalization.NumberStyles.AllowLeadingSign) : off;
-            return new KeyValuePair<int, Insruction>(i, new Insruction(s[0], s[1].Trim(','), off));
-        }).ToDictionary();
-    }
+    public Day23(string input) => Input = Parse(input);
     public Day23(string input, string register)
     {
         Register = register;
-        Input = InputParsers.GetInputLines(input).Select((t, i) =>
+        Input = Parse(input);
+    }
+    private Dictionary<int, Insruction> Parse(string input) => InputParsers.GetInputLines(input).Select((t, i) =>
         {
             var s = t.Split(" ");
             int off = s.Length == 3 ? int.Parse(s[2], System.Globalization.NumberStyles.AllowLeadingSign) : 0;
             off = s[0] == "jmp" ? int.Parse(s[1], System.Globalization.NumberStyles.AllowLeadingSign) : off;
             return new KeyValuePair<int, Insruction>(i, new Insruction(s[0], s[1].Trim(','), off));
         }).ToDictionary();
-    }
+
     public string Output => throw new NotImplementedException();
     private Dictionary<int, Insruction> Input { get; set; }
-    private Dictionary<string, int> Memory { get; set; } = [];
+    private Dictionary<string, int> Memory { get; set; } = new Dictionary<string, int>();
     private string Register { get; set; } = "b";
     public int Star1()
     {
-        Memory.Add("a", 0);
-        Memory.Add("b", 0);
-        var pointer = 0;
-        while (Input.ContainsKey(pointer))
-            pointer = Execute((pointer, Input[pointer])).Invoke();
+        Memory["a"] = 0;
+        Memory["b"] = 0;
+        Run(0);
         return Memory[Register];
     }
     public int Star2()
     {
-        Memory.Clear();
-        Memory.Add("a", 1);
-        Memory.Add("b", 0);
-        var pointer = 0;
-        while (Input.ContainsKey(pointer))
-            pointer = Execute((pointer, Input[pointer])).Invoke();
+        Memory["a"] = 1;
+        Memory["b"] = 0;
+        Run(0);
         return Memory[Register];
     }
 
+    private void Run(int pointer)
+    {
+        while (Input.ContainsKey(pointer))
+            pointer = Execute((pointer, Input[pointer])).Invoke();
+    }
     private Func<int> Execute((int pointer, Insruction inst) t) => t.inst.Ins switch
     {
         "hlf" => () => { Memory[t.inst.Register] /= 2; return t.pointer + 1; },
