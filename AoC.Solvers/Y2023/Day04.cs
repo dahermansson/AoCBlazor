ff71a0cac89c69a99185c91a87e6cf4b196fb272
Hermansson <daniel.hermansson@gmail.com>
2023-12-04 19:06:58 +0100

    Day 4 2023 (#1)

---
+++ AoC.Solvers/Y2023/Day04.cs
@@ -0,0 +1,38 @@
+﻿using AoC.Utils;
+
+namespace AoC.Solvers.Y2023;
+
+public class Day04 : IDay
+{
+    public Day04(string input) => Input = InputParsers.GetInputLines(input).Select(i => new Card(i)).ToArray();
+    public string Output => throw new NotImplementedException();
+
+    private Card[] Input { get; set; }
+
+    public int Star1() => Input.Sum(t => (int)Math.Pow(2, t.Points - 1));
+
+    public int Star2()
+    {
+        var cards = Enumerable.Range(0, Input.Length).Select(t => 1).ToArray();
+        foreach (var card in Input.Select((c, i) => (card: c, id: i)))
+            for (int i = 1; i <= card.card.Points; i++)
+                if (card.id + i < Input.Length)
+                    cards[card.id + i] += 1 * cards[card.id];
+        return cards.Sum();
+    }
+
+    record Card
+    {
+        public Card(string s)
+        {
+            s = s.Replace("   ", " ");
+            s = s.Replace("  ", " ");
+            var splits = s.Split("|");
+            Winning = splits[0].Substring(splits[0].IndexOf(':') + 1).Trim().Split(" ").Select(int.Parse).ToList();
+            You = splits[1].Trim().Split(" ").Select(t => int.Parse(t.Trim())).ToList();
+        }
+        public List<int> Winning { get; set; }
+        public List<int> You { get; set; }
+        public int Points => Winning.Intersect(You).Count();
+    }
+}
--- AoC.Test/Tests_2023.cs
+++ AoC.Test/Tests_2023.cs
@@ -98,4 +98,34 @@ public class Tests_2023
         var actor = new Day03(input);
         Assert.Equal(res, actor.Star2());
     }
+
+    [InlineData("""
+    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    """, 13)]
+    [Theory]
+    public void Day4_Star1(string input, int res)
+    {
+        var actor = new Day04(input);
+        Assert.Equal(res, actor.Star1());
+    }
+
+    [InlineData("""
+    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    """, 30)]
+    [Theory]
+    public void Day4_Star2(string input, int res)
+    {
+        var actor = new Day04(input);
+        Assert.Equal(res, actor.Star2());
+    }
 }
