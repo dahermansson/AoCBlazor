f0006959a9b0a91b51ab831c18751f17b36cbb67
DaHermansson <daniel.hermansson@gmail.com>
2023-12-06 00:04:16 +0100

    Day 5 2023

--- AoC.Solvers/Y2023/Day05.cs
+++ AoC.Solvers/Y2023/Day05.cs
@@ -4,30 +4,88 @@ namespace AoC.Solvers.Y2023;
 
 public class Day05: IDay
 {
-    public Day05(string input) => Input = input;
-    public string Output => throw new NotImplementedException();
+    public Day05(string input)
+    {
+        Input = input;
+        Maps = ParseInput(Input);
+        MapsRev = [];
+        for (int i = Maps.Count-1; i > -1; i--)
+            MapsRev.Add(Maps[i]);
+    }
+    public string Output => output;
 
     private string Input {get; set;}
-    private int[] Seeds;
+    private string output {get; set;} = string.Empty;
+    private long[] Seeds { get; set; } = default!;
+    private List<Map> Maps { get; set;}
+    private List<Map> MapsRev { get; set; }
     public int Star1()
     {
-        ParseInput(Input);
-        return 0;
+        long min = long.MaxValue;
+        foreach (var seed in Seeds)
+        {
+            var dest = GetLocationNr(seed);
+            if(dest < min)
+                min = dest;
+        }
+        output = min.ToString();
+        return -1;
     }
 
     public int Star2()
     {
-        return 0;
+        var seedRanges = new List<(long Start, long End)>();
+        for (int i = 0; i < Seeds.Length/2; i+=2)
+            seedRanges.Add((Seeds[i], Seeds[i] + Seeds[i+1]));
+
+        long seed = 0;
+        long testLocation = 1;
+        bool foundSeed = false;
+        while(true)
+        {
+            seed = GetSeed(testLocation);
+            if(seedRanges.Any(t => t.Start <= seed && t.End >= seed))
+            {
+                foundSeed = true;
+                testLocation -=1;
+            }
+            else
+            {
+                if(foundSeed)
+                    break;
+                testLocation +=1000;
+            }
+        }
+        output = testLocation.ToString();
+        return -1;
     }
 
-    private void ParseInput(string s)
+    private long GetLocationNr(long seed)
+    {
+        var dest = seed;
+        foreach (var map in Maps)
+        {
+            dest = map.GetDestination(dest);
+        }
+        return dest;
+    }
+
+    private long GetSeed(long location)
+    {
+        var seed = location;
+        foreach (var map in MapsRev)
+        {
+            seed = map.GetSeed(seed);
+        }
+        return seed;
+    }
+
+    private List<Map> ParseInput(string s)
     {
         var lines = InputParsers.GetInputLines(s);
-        Seeds = lines.First().Substring(lines.First().IndexOf(":")+1).Trim().Split(" ").Select(t => int.Parse(t.Trim())).ToArray();
+        Seeds = lines.First().Substring(lines.First().IndexOf(":")+1).Trim().Split(" ").Select(t => long.Parse(t.Trim())).ToArray();
 
-        var maps = s.Split($"{Environment.NewLine}{Environment.NewLine}").Skip(1).Select(t => new Map(t)).ToList();
-
-
+        return s.Split($"{Environment.NewLine}{Environment.NewLine}").Skip(1).Select(t => new Map(t)).ToList();
     }
 
     record Map
@@ -39,21 +97,32 @@ public class Day05: IDay
         {
             var lines = s.Split(Environment.NewLine);
             Name = lines[0].Replace(" map:", "");
-            Ranges = lines.Skip(1).Select(t => { var n = t.Split(" ").Select(int.Parse).ToArray(); return new Range(n[0], n[1], n[2]);}).ToList();
+            Ranges = lines.Skip(1).Select(t => { var n = t.Split(" ").Select(long.Parse).ToArray(); return new Range(n[0], n[1], n[2]);}).ToList();
         }
         
-        public int GetDestination(int seedNr)
+        public long GetDestination(long seedNr)
         {
-            var r = Ranges.SingleOrDefault(t => t.GetSource.start <= seedNr && seedNr <= t.GetSource.stop);
+            var r = Ranges.FirstOrDefault(t => t.GetSource.start <= seedNr && seedNr <= t.GetSource.stop);
             if(r != null)
-                return 0;
+                return r.DestStart + (seedNr - r.SourceStart);
             else
-                return 0;
+                return seedNr;
+        }
+
+        public long GetSeed(long location)
+        {
+            var r = Ranges.FirstOrDefault(t => t.GetDest.start <= location && location <= t.GetDest.stop);
+            if(r != null)
+                return r.SourceStart + (location - r.DestStart);
+            else
+                return location;
         }
 
     }
-    record Range(int DestStart, int SourceStart, int Length)
+    record Range(long DestStart, long SourceStart, long Length)
     {
-        public (int start, int stop) GetSource => (SourceStart, SourceStart+Length);
+        public (long start, long stop) GetSource => (SourceStart, SourceStart+Length);
+        public (long start, long stop) GetDest => (DestStart, DestStart+Length);
+
     }
 }
--- AoC.Test/Tests_2023.cs
+++ AoC.Test/Tests_2023.cs
@@ -128,4 +128,91 @@ public class Tests_2023
         var actor = new Day04(input);
         Assert.Equal(res, actor.Star2());
     }
+
+    [InlineData("""
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """, "35")]
+    [Theory]
+    public void Day5_Star1(string input, string res)
+    {
+        var actor = new Day05(input);
+        _ = actor.Star1();
+        Assert.Equal(res, actor.Output);
+    }
+
+    [InlineData("""
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """, "46")]
+    
+    [Theory(Skip = "bad solution :(")]
+    public void Day5_Star2(string input, string res)
+    {
+        var actor = new Day05(input);
+        _ = actor.Star2();
+        Assert.Equal(res, actor.Output);
+    }
 }
