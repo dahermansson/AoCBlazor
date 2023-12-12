f0c71b101f9eea258141fbae086164df2d66d731
DaHermansson <daniel.hermansson@gmail.com>
2023-12-13 00:05:27 +0100

    Day 12 working

--- AoC.Solvers/Y2023/Day12.cs
+++ AoC.Solvers/Y2023/Day12.cs
@@ -1,60 +1,66 @@
 ﻿namespace AoC.Solvers.Y2023;
 
-public class Day12: IDay
+public class Day12 : IDay
 {
     public Day12(string input) => Input = InputParsers.GetInputLines(input);
-    public string Output => throw new NotImplementedException();
-
-    private string[] Input {get; set;}
+    public string Output => output;
+    private string output { get; set; } = default!;
+    private string[] Input { get; set; }
 
     public int Star1()
     {
-        var sum = 0;
-        Input = ["?#?#?#?#?#?#?#? 1,3,1,6"];
-
+        long sum = 0;
         foreach (var row in Input)
         {
             var spring = row.Split(" ")[0];
-            var unknowns = spring.IndexOfMany(c => c == '?').ToArray();
             var brokenGroups = row.Split(" ")[1].Split(',').Select(int.Parse).ToArray();
-            var res = new List<char[]>();
-            GetArrangements([..spring], unknowns, res);
-            
-            var hepp = res.Where(k => IsValid(string.Concat(k), brokenGroups)).ToList();
+            sum += Cache(spring, brokenGroups);
         }
-        return sum;
+        output = sum.ToString();
+        return -1;
     }
     public int Star2()
     {
-        return 0;
+        long sum = 0;
+        foreach (var row in Input)
+        {
+            var spring = row.Split(" ")[0];
+            var brokenGroups = row.Split(" ")[1].Split(',').Select(int.Parse).ToArray();
+            spring = string.Join('?', Enumerable.Range(0, 5).Select(t => spring));
+            brokenGroups = Enumerable.Repeat(brokenGroups, 5).SelectMany(t => t).ToArray();
+            sum += Cache(spring, brokenGroups);
+        }
+        output = sum.ToString();
+        return -1;
+    }
+    private Dictionary<string, long> Lut { get; set; } = new();
+    private long Cache(string s, int[] groups)
+    {
+        var key = $"{s}{string.Join('.', groups)}";
+        if (!Lut.ContainsKey(key))
+            Lut[key] = CountValid(s, groups);
+        return Lut[key];
     }
 
-    private void GetArrangements(char[] s, int[] unknowns, List<char[]> res)
+    private long CountValid(string s, int[] groups)
     {
-        if(!s.Any(t => t == '?'))
-        {
-            res.Add(s);
-            return;
-        }
-        else
-        {
-            foreach (var unknown in unknowns)
-            {
-                s[unknown] = '.';
-                GetArrangements([..s], [..unknowns.Skip(1)], res);
-                s[unknown] = '#';
-                GetArrangements([..s], [..unknowns.Skip(1)], res);
-            }
-        }
-        
-    }
+        if (groups.Length == 0)
+            return s.Contains("#") ? 0 : 1;
+        if (s == string.Empty)
+            return 0;
+        if (s[0] == '.')
+            return Cache(s.Substring(1), groups);
+        if (s[0] == '?')
+            return Cache($".{s[1..]}", groups) + Cache($"#{s[1..]}", groups);
 
-    private bool IsValid(string springs, int[] brokenGroups)
-    {
-        var broken = springs.Split('.').Where(t => t.All(c => c == '#') && t.Length != 0).ToArray();
-        for(int i = 0; i <broken.Length; i++)
-            if(broken[i].Length != brokenGroups[i])
-                return false;
-        return true;
+        if (groups.Length == 0 || s.Length < groups[0] || s[..groups[0]].Contains('.'))
+            return 0;
+        if (groups.Length > 1)
+        {
+            if (s.Length < groups[0] + 1 || s[groups[0]] == '#')
+                return 0;
+            return Cache(s.Substring(groups[0] + 1), groups[1..]);
+        }
+        return Cache(s.Substring(groups[0]), groups[1..]);
     }
 }
