783f38433ede5eee7950cba247de6a884d346793
DaHermansson <daniel.hermansson@gmail.com>
2023-12-18 00:01:13 +0100

    fixed start pos

--- AoC.Solvers/Y2023/Day17.cs
+++ AoC.Solvers/Y2023/Day17.cs
@@ -9,23 +9,17 @@ public class Day17 : IDay
         for (int row = 0; row < Input.Length; row++)
             for (int col = 0; col < Input[row].Length; col++)
                 Map[(row, col)] = int.Parse(Input[row][col].ToString());
+        Goal = (Map.Keys.Max(k => k.X), Map.Keys.Max(k => k.Y));
     }
-    private Dictionary<(int X, int Y), int> Map { get; set; }
+    private Dictionary<(int X, int Y), int> Map { get; init; }
     public string Output => throw new NotImplementedException();
+    private (int X, int Y) Goal { get; init; }
+    private string[] Input { get; init; }
 
-    private string[] Input { get; set; }
+    public int Star1() => MinimizeHeat(new(0, 0, -1, -1), false);
+    public int Star2() => MinimizeHeat(new(0, 0, -1, -1), true);
 
-    public int Star1() => new Pos[] {
-            new(0, 0, 0, -1), 
-            new(0, 0, 2, -1) 
-            }.Min(t => FindWay(t, Map.Max(t => t.Key.X), Map.Max(t => t.Key.Y), false));
-    
-    public int Star2() => new Pos[] {
-            new(0, 0, 0, -1), 
-            new(0, 0, 2, -1) 
-            }.Min(t => FindWay(t, Map.Max(t => t.Key.X), Map.Max(t => t.Key.Y), true));
-
-    private int FindWay(Pos start, int goalX, int goalY, bool ultraCrucibles)
+    private int MinimizeHeat(Pos start, bool ultraCrucibles)
     {
         HashSet<Pos> v = [];
         PriorityQueue<Pos, int> q = new();
@@ -35,15 +29,13 @@ public class Day17 : IDay
             if (v.Contains(current))
                 continue;
             v.Add(current);
-            if(ultraCrucibles)
-            {
-                if (current.Row == goalX && current.Col == goalY && current.DirBlockCount >= 3)
-                    return heat;
-            }   
-            else if (current.Row == goalX && current.Col == goalY)
-                    return heat;
-               
-            foreach (var next in ultraCrucibles ? GetNextUltraBlock(current) : GetNext(current))
+
+            if (!ultraCrucibles && current.Row == Goal.X && current.Col == Goal.Y)
+                return heat;
+            if (current.Row == Goal.X && current.Col == Goal.Y && current.DirBlockCount >= 3)
+                return heat;
+
+            foreach (var next in GetNext(current, ultraCrucibles))
             {
                 if (v.Contains(next) || !Map.ContainsKey((next.Row, next.Col)))
                     continue;
@@ -53,37 +45,28 @@ public class Day17 : IDay
         return 0;
     }
 
-    private IEnumerable<Pos> GetNext(Pos c)
+    private IEnumerable<Pos> GetNext(Pos c, bool ultraCrucibles)
     {
-        if (c.DirBlockCount < 2 )
-            yield return new Pos(c.Row + Dir[c.Dir].X, c.Col + Dir[c.Dir].Y, c.Dir, c.DirBlockCount + 1);
-        if (c.Dir < 2)
-        {
-            yield return new Pos(c.Row + Dir[2].X, c.Col + Dir[2].Y, 2, 0);
-            yield return new Pos(c.Row + Dir[3].X, c.Col + Dir[3].Y, 3, 0);
-        }
-        else if (c.Dir > 1)
+        if (c.Dir == -1)
         {
             yield return new Pos(c.Row + Dir[0].X, c.Col + Dir[0].Y, 0, 0);
-            yield return new Pos(c.Row + Dir[1].X, c.Col + Dir[1].Y, 1, 0);
+            yield return new Pos(c.Row + Dir[2].X, c.Col + Dir[2].Y, 0, 0);
         }
-    }
-    private IEnumerable<Pos> GetNextUltraBlock(Pos c)
-    {
-        if (c.DirBlockCount < 9)
-            yield return new Pos(c.Row + Dir[c.Dir].X, c.Col + Dir[c.Dir].Y, c.Dir, c.DirBlockCount + 1);
-        if (c.DirBlockCount >= 3)
+        else
         {
-            if(c.Dir < 2)
-            {
-                yield return new Pos(c.Row + Dir[2].X, c.Col + Dir[2].Y, 2, 0);
-                yield return new Pos(c.Row + Dir[3].X, c.Col + Dir[3].Y, 3, 0);
-            }
-            else if(c.Dir > 1)
-            {
-                yield return new Pos(c.Row + Dir[0].X, c.Col + Dir[0].Y, 0, 0);
-                yield return new Pos(c.Row + Dir[1].X, c.Col + Dir[1].Y, 1, 0);
-            }
+            if (c.DirBlockCount < (ultraCrucibles ? 9 : 2))
+                yield return new Pos(c.Row + Dir[c.Dir].X, c.Col + Dir[c.Dir].Y, c.Dir, c.DirBlockCount + 1);
+            if (c.DirBlockCount >= (ultraCrucibles ? 3 : 0))
+                if (c.Dir < 2)
+                {
+                    yield return new Pos(c.Row + Dir[2].X, c.Col + Dir[2].Y, 2, 0);
+                    yield return new Pos(c.Row + Dir[3].X, c.Col + Dir[3].Y, 3, 0);
+                }
+                else
+                {
+                    yield return new Pos(c.Row + Dir[0].X, c.Col + Dir[0].Y, 0, 0);
+                    yield return new Pos(c.Row + Dir[1].X, c.Col + Dir[1].Y, 1, 0);
+                }
         }
     }
     record Pos(int Row, int Col, int Dir, int DirBlockCount);
