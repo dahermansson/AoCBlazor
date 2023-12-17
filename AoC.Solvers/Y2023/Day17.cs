85107870de179ef8a9ae4e5d8d0310891b0584e5
DaHermansson <daniel.hermansson@gmail.com>
2023-12-17 19:06:34 +0100

    Day 17 2023 not working with test case

--- AoC.Solvers/Y2023/Day17.cs
+++ AoC.Solvers/Y2023/Day17.cs
@@ -1,6 +1,4 @@
-﻿using System.Numerics;
-
-namespace AoC.Solvers.Y2023;
+﻿namespace AoC.Solvers.Y2023;
 
 public class Day17 : IDay
 {
@@ -20,33 +18,31 @@ public class Day17 : IDay
 
     public int Star1()
     {
-        //return 0;
-        return new Pos[] {
-            new Pos(0, 0, 0, 1), 
-            new Pos(0, 0, 2, 1) 
-            }.Min(m => FindWay(m, Map.Max(t => t.Key.X), Map.Max(t => t.Key.Y), false));
+        var starts = new Pos[] {
+            new Pos(0, 0, 0, 0), 
+            new Pos(0, 0, 2, 0) 
+            };
+        var res = new List<int>();
+        foreach (var start in starts)
+            res.Add(FindWay(start, Map.Max(t => t.Key.X), Map.Max(t => t.Key.Y), false));
+        
+        return res.Min();
     }
     public int Star2()
     {
-        var t = new Pos[] {
-            new Pos(0, 0, 0, 1), 
-            new Pos(0, 0, 2, 1) 
-            }.Min(m => FindWay(m, Map.Max(t => t.Key.X), Map.Max(t => t.Key.Y), true));
-        //foreach (var a in t)
-        //{
-          //  Console.WriteLine(a);
-        //}
-        return t;
+        var starts = new Pos[] {
+            new Pos(0, 0, 0, 0), 
+            new Pos(0, 0, 2, 0) 
+            };
+        var res = new List<int>();
+        foreach (var start in starts)
+            res.Add(FindWay(start, Map.Max(t => t.Key.X), Map.Max(t => t.Key.Y), true));
+        
+        return res.Min();
     }
 
-    private int FindWay(Pos start, int goalX, int goalY, bool ultraBlock)
+    private int FindWay(Pos start, int goalX, int goalY, bool ultraCrucibles)
     {
-        //954 to low
-        //1144 to low
-        //1148 wrong
-        //1149 Correct
-        //1152 wrong
-        //1188 to high
         HashSet<Pos> v = [];
         PriorityQueue<Pos, int> q = new();
         q.Enqueue(start, 0);
@@ -55,59 +51,65 @@ public class Day17 : IDay
             if (v.Contains(current))
                 continue;
             v.Add(current);
-            if(ultraBlock)
+            if(ultraCrucibles)
             {
-                if (current.Row == goalX && current.Col == goalY && current.DirBlockCount == 5)
+                //Test case works with DirBlockCount >= 3 and Input gives 1144 (wrong answer)
+                //if (current.Row == goalX && current.Col == goalY && current.DirBlockCount >= 3)
+                
+                //Only DirBlockCount == 5 gives correct answer. 
+                //Both DirBlocCount >= 3 and DirBlockCount >= 4 gives 1144
+                //The test case dont work with DirBlockCount == 4
+                if (current.Row == goalX && current.Col == goalY && current.DirBlockCount == 4)
                     return heat;
             }   
             else
+            {
                 if (current.Row == goalX && current.Col == goalY)
                     return heat;
-                
-            foreach (var next in ultraBlock ? GetNextUltraBlock(current).ToArray() : GetNext(current).ToArray())
+            }   
+            foreach (var next in ultraCrucibles ? GetNextUltraBlock(current).ToArray() : GetNext(current).ToArray())
             {
                 if (v.Contains(next) || !Map.ContainsKey((next.Row, next.Col)))
                     continue;
                 int nextHeat = heat + Map[(next.Row, next.Col)];
                 q.Enqueue(next, nextHeat);
             }
-
         }
         return 0;
     }
 
     private IEnumerable<Pos> GetNext(Pos c)
     {
-        if (c.DirBlockCount < 3)
+        if (c.Dir < 2)
+        {
+            yield return new Pos(c.Row + Dir[2].X, c.Col + Dir[2].Y, 2, 0);
+            yield return new Pos(c.Row + Dir[3].X, c.Col + Dir[3].Y, 3, 0);
+        }
+        else if (c.Dir > 1)
+        {
+            yield return new Pos(c.Row + Dir[0].X, c.Col + Dir[0].Y, 0, 0);
+            yield return new Pos(c.Row + Dir[1].X, c.Col + Dir[1].Y, 1, 0);
+        }
+        if (c.DirBlockCount < 2 )
             yield return new Pos(c.Row + Dir[c.Dir].X, c.Col + Dir[c.Dir].Y, c.Dir, c.DirBlockCount + 1);
-        if (c.Dir == 0 || c.Dir == 1)
-        {
-            yield return new Pos(c.Row + Dir[2].X, c.Col + Dir[2].Y, 2, 1);
-            yield return new Pos(c.Row + Dir[3].X, c.Col + Dir[3].Y, 3, 1);
-        }
-        else if (c.Dir == 2 || c.Dir == 3)
-        {
-            yield return new Pos(c.Row + Dir[0].X, c.Col + Dir[0].Y, 0, 1);
-            yield return new Pos(c.Row + Dir[1].X, c.Col + Dir[1].Y, 1, 1);
-        }
     }
     private IEnumerable<Pos> GetNextUltraBlock(Pos c)
     {
-        if (c.DirBlockCount < 10) // can go straight
-            yield return new Pos(c.Row + Dir[c.Dir].X, c.Col + Dir[c.Dir].Y, c.Dir, c.DirBlockCount + 1);
-        if (c.DirBlockCount >= 4) // allow turn
+        if (c.DirBlockCount >= 3) // allow turn
         {
-            if(c.Dir == 0 || c.Dir == 1)
+            if(c.Dir < 2)
             {
-                yield return new Pos(c.Row + Dir[2].X, c.Col + Dir[2].Y, 2, 1);
-                yield return new Pos(c.Row + Dir[3].X, c.Col + Dir[3].Y, 3, 1);
+                yield return new Pos(c.Row + Dir[2].X, c.Col + Dir[2].Y, 2, 0);
+                yield return new Pos(c.Row + Dir[3].X, c.Col + Dir[3].Y, 3, 0);
             }
-            else if(c.Dir == 2 || c.Dir == 3)
+            else if(c.Dir > 1)
             {
-                yield return new Pos(c.Row + Dir[0].X, c.Col + Dir[0].Y, 0, 1);
-                yield return new Pos(c.Row + Dir[1].X, c.Col + Dir[1].Y, 1, 1);
+                yield return new Pos(c.Row + Dir[0].X, c.Col + Dir[0].Y, 0, 0);
+                yield return new Pos(c.Row + Dir[1].X, c.Col + Dir[1].Y, 1, 0);
             }
         }
+        if (c.DirBlockCount < 9) // can go straight
+            yield return new Pos(c.Row + Dir[c.Dir].X, c.Col + Dir[c.Dir].Y, c.Dir, c.DirBlockCount + 1);
     }
     record Pos(int Row, int Col, int Dir, int DirBlockCount);
     private readonly List<(int X, int Y)> Dir = [
