5414a01e56d1c6b76e53f261befefd14c8a5e937
DaHermansson <daniel.hermansson@gmail.com>
2024-10-20 01:13:46 +0200

    Day 5 2017

---
+++ AoC.Solvers/Y2017/Day05.cs
@@ -0,0 +1,37 @@
+﻿namespace AoC.Solvers.Y2017;
+
+public class Day05(string input) : IDay
+{
+    public string Output => throw new NotImplementedException();
+
+    private int[] Input { get; set; } = InputParsers.GetInputLines<int>(input);
+
+    public int Star1()
+    {
+        var jumps = Input.Select((v, i) => (Key: i, Value: v)).ToDictionary(a => a.Key, a => a.Value);
+        int index = 0;
+        int turns = 0;
+        do
+        {
+            var jump = jumps[index]++;
+            index += jump;
+            turns++;
+        } while (jumps.ContainsKey(index));
+        return turns;
+    }
+
+    public int Star2()
+    {
+        var jumps = Input.Select((v, i) => (Key: i, Value: v)).ToDictionary(a => a.Key, a => a.Value);
+        int index = 0;
+        int turns = 0;
+        do
+        {
+            var jump = jumps[index];
+            jumps[index] = jump >= 3 ? jumps[index] - 1 : jumps[index] + 1;
+            index += jump;
+            turns++;
+        } while (jumps.ContainsKey(index));
+        return turns;
+    }
+}
