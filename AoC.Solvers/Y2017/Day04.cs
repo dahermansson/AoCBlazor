80a4a77b4e33b0e21a019d890e483862d85315df
DaHermansson <daniel.hermansson@gmail.com>
2024-10-20 00:46:14 +0200

    Day4 2017

---
+++ AoC.Solvers/Y2017/Day04.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Toolkit.HighPerformance;
+
+namespace AoC.Solvers.Y2017;
+
+public class Day04(string input) : IDay
+{
+    public string Output => throw new NotImplementedException();
+    private string[] Input { get; set; } = InputParsers.GetInputLines(input);
+
+    public int Star1() => Input.Count(t => t.Split(" ").Length == t.Split(" ").GroupBy(w => w).Count());
+
+    public int Star2()
+    {
+        bool HasNotAnagram(string pwd)
+        {
+            var words = pwd.Split(" ").Select((t, i) => (Index: i, Word: t));
+            return !words.Any(a => words.Any(b => a.Index != b.Index && string.Concat(a.Word.Order()) == string.Concat(b.Word.Order())));
+        }
+        return Input.Count(HasNotAnagram);
+    }
+}
