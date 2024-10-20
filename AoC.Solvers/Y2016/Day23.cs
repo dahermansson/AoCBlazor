c93767bd23259d52ab2452e03b84b01d89227a37
DaHermansson <daniel.hermansson@gmail.com>
2024-10-20 17:27:19 +0200

    Day 23 2016

--- AoC.Solvers/Y2016/Day23.cs
+++ AoC.Solvers/Y2016/Day23.cs
@@ -1,51 +1,64 @@
-﻿namespace AoC.Solvers.Y2016;
+﻿using SixLabors.ImageSharp;
 
-public class Day23: IDay
+namespace AoC.Solvers.Y2016;
+
+public class Day23(string input) : IDay
 {
-    public Day23(string input) => Input = input;
     public string Output => throw new NotImplementedException();
 
-    private string Input {get; set;}
+    private string[] Input { get; set; } = InputParsers.GetInputLines(input);
 
     public int Star1()
     {
-        return 0;
+        var computer = new Computer(Input);
+        computer.Register["a"] = 7;
+        computer.Run();
+        return computer.Register["a"];
     }
 
     public int Star2()
     {
-        return 0;
+        var computer = new Computer(Input);
+        computer.Register["a"] = 12;
+        computer.Run();
+        return computer.Register["a"];
     }
 
-    private class Computer
+    private class Computer(string[] instructions)
     {
         public Dictionary<string, int> Register { get; set; } = new() { { "a", 0 }, { "b", 0 }, { "c", 0 }, { "d", 0 } };
         private int Pointer { get; set; } = 0;
-        private string[] Instructions { get; set; }
-        public Computer(string[] instructions)
-        {
-            Instructions = instructions;
-        }
+        private string[] Instructions { get; set; } = [.. instructions];
+
         public void Run()
         {
             while (Pointer > -1 && Pointer < Instructions.Length)
             {
+                if (Pointer == 4) //Just skip the multiply loop and multiply
+                {
+                    Register["a"] = Register["b"] * Register["d"];
+                    Register["c"] = 0;
+                    Register["d"] = 0;
+                    Pointer += 6;
+                }
                 var inst = Instructions[Pointer].Split(" ");
                 if (inst[0] == "cpy")
                     Pointer = Cpy(Pointer, inst[1], inst[2]);
-                if (inst[0] == "jnz")
+                else if (inst[0] == "jnz")
                     Pointer = Jnz(Pointer, inst[1], inst[2]);
-                if (inst[0] == "inc")
+                else if (inst[0] == "inc")
                     Pointer = Inc(Pointer, inst[1]);
-                if (inst[0] == "dec")
+                else if (inst[0] == "dec")
                     Pointer = Dec(Pointer, inst[1]);
-                if (inst[0] == "tgl")
+                else if (inst[0] == "tgl")
                     Pointer = Tgl(Pointer, inst[1]);
             }
         }
 
         public int Cpy(int p, string x, string y)
         {
+            if (int.TryParse(y, out int _))
+                return p + 1;
             if (int.TryParse(x, out int v))
                 Register[y] = v;
             else
@@ -64,14 +77,51 @@ public class Day23: IDay
         }
         public int Jnz(int p, string x, string y)
         {
+            if (!int.TryParse(y, out int j))
+                j = Register[y];
             if (int.TryParse(x, out int v) && v != 0 || Register[x] != 0)
-                return p + int.Parse(y, System.Globalization.NumberStyles.AllowLeadingSign);
+                return p + j;
             return p + 1;
         }
 
         public int Tgl(int p, string a)
         {
-            if(Register[a] == 0)
+            var togleInstruction = p + Register[a];
+            if (togleInstruction < 0 || togleInstruction >= Instructions.Length)
+                return p + 1;
+            if (togleInstruction == 0)
+            {
+                Instructions[togleInstruction] = Instructions[togleInstruction].Replace("tgl", "inc");
+                return p + 1;
+            }
+            var togle = Instructions[togleInstruction].Split(" ");
+            if (togle.Length == 2)
+            {
+                if (togle[0] == "inc")
+                {
+                    Instructions[togleInstruction] = Instructions[togleInstruction].Replace("inc", "dec");
+                    return p + 1;
+                }
+                else
+                {
+                    Instructions[togleInstruction] = Instructions[togleInstruction].Replace(togle[0], "inc");
+                    return p + 1;
+                }
+            }
+            if (togle.Length == 3)
+            {
+                if (togle[0] == "jnz")
+                {
+                    Instructions[togleInstruction] = Instructions[togleInstruction].Replace("jnz", "cpy");
+                    return p + 1;
+                }
+                else
+                {
+                    Instructions[togleInstruction] = Instructions[togleInstruction].Replace(togle[0], "jnz");
+                    return p + 1;
+                }
+            }
+            return p + 1;
         }
     }
 }
