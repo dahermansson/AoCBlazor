d98467eeea4ad6d41a3692b8d0e7954f2427e09a
DaHermansson <daniel.hermansson@gmail.com>
2024-12-06 22:42:36 +0100

    beter loop-check

--- AoC.Solvers/Y2024/Day06.cs
+++ AoC.Solvers/Y2024/Day06.cs
@@ -5,22 +5,21 @@ public class Day06(string input) : IDay
     public string Output => throw new NotImplementedException();
     private string[] Input { get; set; } = InputParsers.GetInputLines(input);
     private readonly (int x, int y)[] Dir = [(-1, 0), (0, 1), (1, 0), (0, -1)];
-    private readonly (int x, int y) Start = InputParsers.GetInputLines(input).Select((row, x) => (row, x)).Where((t) => t.row.Contains('^')).Select(t => (t.x, y: t.row.IndexOf('^'))).Single();
+    private readonly (int x, int y, int dir) Start = InputParsers.GetInputLines(input).Select((row, x) => (row, x)).Where((t) => t.row.Contains('^')).Select(t => (t.x, t.row.IndexOf('^'), 0)).Single();
 
-    bool IsBlockCreatingLoop((int x, int y) block) => GetGuardsPath(block).Count == 0; 
+    bool IsBlockCreatingLoop((int x, int y, int dir) block) => GetGuardsPath(block).Count == 0; 
     
-    private HashSet<(int x, int y)> GetGuardsPath((int x, int y)? block = null)
+    private HashSet<(int x, int y, int dir)> GetGuardsPath((int x, int y, int dir)? block = null)
     {
         var currentDir = 0;
         var current = Start;
-        HashSet<(int x, int y)> positions = [current];
-        var stepsSensLastAdded = 0;
+        HashSet<(int x, int y, int dir)> positions = [current];
         while (true)
         {
             var possibleNext = (x: current.x + Dir[currentDir].x, y: current.y + Dir[currentDir].y);
             if (possibleNext.x >= Input.Length || possibleNext.x < 0
                 || possibleNext.y >= Input[0].Length || possibleNext.y < 0)
-                return positions;
+                return [.. positions.DistinctBy(t => (t.x, t.y))];
 
             while (Input[possibleNext.x][possibleNext.y] == '#'
                    || (block != null && block.Value.x == possibleNext.x && block.Value.y == possibleNext.y))
@@ -28,9 +27,8 @@ public class Day06(string input) : IDay
                 currentDir = (currentDir + 1) % Dir.Length;
                 possibleNext = (current.x + Dir[currentDir].x, current.y + Dir[currentDir].y);
             }
-            current = (current.x + Dir[currentDir].x, current.y + Dir[currentDir].y);
-            stepsSensLastAdded = positions.Add(current) ? 0 : stepsSensLastAdded + 1;
-            if (stepsSensLastAdded > positions.Count)
+            current = (current.x + Dir[currentDir].x, current.y + Dir[currentDir].y, currentDir);
+            if(!positions.Add(current))
                 return [];
         }
     }
