d10cfd846c3391da48d133e9c2a1550584a5ba81
DaHermansson <daniel.hermansson@gmail.com>
2024-12-12 18:55:48 +0100

    a

--- AoC.Solvers/Y2024/Day12.cs
+++ AoC.Solvers/Y2024/Day12.cs
@@ -6,67 +6,59 @@ public class Day12(string input) : IDay
 
     private string[] Input { get; set; } = InputParsers.GetInputLines(input);
 
-
     private static readonly List<(int x, int y)> Dir = [(-1,0), (0, 1), (1, 0), (0,-1)];
-    private List<ValuePos> GetNeigbours(ValuePos valuePos) => 
+    private List<Plot> GetNeighbors(Plot valuePos) => 
         Dir.Where(d => valuePos.X + d.x < Input.Length &&
             valuePos.X + d.x > -1 && 
             valuePos.Y + d.y < Input[0].Length &&
-            valuePos.Y + d.y > -1).Select(d => 
-            new ValuePos(valuePos.X + d.x, valuePos.Y + d.y, Input[valuePos.X + d.x][valuePos.Y + d.y])
+            valuePos.Y + d.y > -1 && Input[valuePos.X + d.x][valuePos.Y + d.y] == valuePos.Value).Select(d => 
+            new Plot(valuePos.X + d.x, valuePos.Y + d.y, Input[valuePos.X + d.x][valuePos.Y + d.y])
             ).ToList();
 
-    record ValuePos(int X, int Y, char Value)
+    record Plot(int X, int Y, char Value)
     {
         public (int X, int Y) Pos => (X, Y);
+    }
+
+    private List<List<Plot>> GetRegions()
+    {
+        IEnumerable<Plot> CreateRegion(Plot current, HashSet<Plot> garden, HashSet<(int x, int Y)> notInRegions)
+        {
+            if(garden.Count == 0)
+            {
+                garden.Add(current);
+                notInRegions.Remove(current.Pos);
+            }
+
+            var neighbors = GetNeighbors(current).Where(n => notInRegions.Contains(n.Pos) && !garden.Contains(n)).ToList();
+            
+            if(neighbors.Count == 0)
+                return garden;
+
+            foreach (var neighbor in neighbors)
+            {
+                garden.Add(neighbor);
+                notInRegions.Remove(neighbor.Pos);
+                CreateRegion(neighbor, garden, notInRegions);
+            }
+            return garden;
+        }
+
+        var garden = Input.Select((row, x) => (row, x)).SelectMany(row => row.row.Select((c, y) => new Plot(row.x, y, row.row[y]))).ToDictionary(key => key.Pos, value => value);
+        var regions = new List<List<Plot>>();
+        HashSet<(int X, int Y)> notInRegions = [];
+        garden.Keys.ToList().ForEach(p => notInRegions.Add(p));
+        while(notInRegions.Count != 0)
+        {
+            regions.Add(CreateRegion(garden[notInRegions.First()], [], notInRegions).ToList());
+        }
+        return regions;
     }
     
     public int Star1()
     {
-        Input = """
-        RRRRIICCFF
-        RRRRIICCCF
-        VVRRRCCFFF
-        VVRCCCJFFF
-        VVVVCJJCFE
-        VVIVCCJJEE
-        VVIIICJJEE
-        MIIIIIJJEE
-        MIIISIJEEE
-        MMMISSJEEE
-        """.Split(Environment.NewLine);
-        var garden = Input.Select((row, x) => (row, x)).SelectMany(row => row.row.Select((c, y) => new ValuePos(row.x, y, row.row[y]))).ToDictionary(key => key.Pos, value => value);
-
-        var regions = new List<List<ValuePos>>();
-        foreach(var g in garden.GroupBy(t => t.Value.Value))
-        {
-            var k = g.Aggregate(new List<List<ValuePos>>(), (r, plot) => {
-                var n = GetNeigbours(plot.Value).Where(t => t.Value == plot.Value.Value);
-                try{
-
-                
-                var nRegion = r.Where(t => n.Any(c => t.Contains(c))).SingleOrDefault();
-                if(nRegion != null)
-                {
-                    nRegion.Add(plot.Value);
-                }
-                else
-                {
-                    nRegion = [plot.Value];
-                    r.Add(nRegion);
-                }
-                }
-                catch(Exception e)
-                {
-                    
-                }
-                return r;
-            });
-            regions.AddRange(k);
-        }
-
-
-        return regions.Sum(t => t.Count * t.Sum(n => 4 - GetNeigbours(n).Count(c => c.Value == n.Value)));
+        var regions = GetRegions();
+        return regions.Sum(t => t.Count * t.Sum(n => 4 - GetNeighbors(n).Count(c => c.Value == n.Value)));
     }
 
     public int Star2()
