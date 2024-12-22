ca6dd48878e4a844e7e17922b46be153790803e9
DaHermansson <daniel.hermansson@gmail.com>
2024-12-22 23:56:05 +0100

    Day 22

---
+++ AoC.Solvers/Y2024/Day22.cs
@@ -0,0 +1,66 @@
+﻿namespace AoC.Solvers.Y2024;
+
+public class Day22(string input) : IDay
+{
+    public string Output => output;
+    private string output = string.Empty;
+
+    private long[] Input { get; set; } = InputParsers.GetInputLines(input).Select(long.Parse).ToArray();
+
+    private static long Mix(long secret, long mix) => secret ^ mix;
+    private static long Prun(long secret) => secret % 16777216;
+    private static long Op1(long secret) => Prun(Mix(secret, secret * 64));
+    private static long Op2(long secret) => Prun(Mix(secret, secret / 32));
+    private static long Op3(long secret) => Prun(Mix(secret, secret * 2048));
+    
+    private static long GetSecretNumber(long secret)
+    {
+        secret = Op1(secret);
+        secret = Op2(secret);
+        return Op3(secret);
+    }
+
+    private static IEnumerable<(int Bananas, int Diff)> GetPrice(long secretNumber)
+    {
+        yield return ((int)(secretNumber % 10), 0);
+        var prevSecretNumber = secretNumber;
+        for (var i = 1; i < 2000; i++)
+        {
+            secretNumber = GetSecretNumber(secretNumber);
+
+            int bananas = (int)(secretNumber % 10);
+            int prevBananas = (int)(prevSecretNumber % 10);
+            yield return (bananas, bananas - prevBananas);
+            prevSecretNumber = secretNumber;
+        }
+    }
+
+    public int Star1()
+    {
+        output = Input.Sum(t => Enumerable.Range(0, 2000).Aggregate(t, (a, b) => GetSecretNumber(a))).ToString();
+        return -1;
+    }
+
+    public int Star2()
+    {
+        Dictionary<(int a, int b, int c, int d), int> sequences = [];
+        void PriceOnChanges((int Bananas, int Diff)[] prices)
+        {
+            HashSet<(int a, int b, int c, int d)> seen = [];
+            foreach(var t in prices.Select((Price, Index) => (Price, Index)).Skip(4))
+            {
+                var sequence = (prices[t.Index - 3].Diff, prices[t.Index - 2].Diff, prices[t.Index - 1].Diff, prices[t.Index].Diff);
+                if (seen.Contains(sequence))
+                    continue;
+
+                sequences.TryGetValue(sequence, out var value);
+                sequences[sequence] = value + t.Price.Bananas;
+                seen.Add(sequence);
+            }
+        }
+
+        Input.ToList().ForEach(t => PriceOnChanges(GetPrice(t).ToArray()));
+
+        return sequences.Max(t => t.Value);
+    }
+}
