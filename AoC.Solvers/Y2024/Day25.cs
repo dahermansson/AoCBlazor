2c54052534e1727d97a2d641ada9d04f3d77f65a
DaHermansson <daniel.hermansson@gmail.com>
2024-12-25 11:12:55 +0100

    Day 25

---
+++ AoC.Solvers/Y2024/Day25.cs
@@ -0,0 +1,27 @@
+﻿namespace AoC.Solvers.Y2024;
+
+public class Day25(string input) : IDay
+{
+    public string Output => throw new NotImplementedException();
+
+    private IEnumerable<(int[] Pins, bool IsLock)> LocksAndKeys { get; set; } = input.Split($"{Environment.NewLine}{Environment.NewLine}").Select(g =>
+        {
+            var isLock = g.StartsWith("#####");
+            string[] lines = isLock ? [.. g.Split(Environment.NewLine)] : [.. g.Split(Environment.NewLine).Reverse()];
+
+            int[] pins = Enumerable.Repeat(-1, 5).ToArray();
+            foreach (var line in lines.Skip(1).Index())
+                foreach (var pin in line.Item.Index())
+                    if (pins[pin.Index] == -1 && pin.Item == '.')
+                        pins[pin.Index] = line.Index;
+
+            return (Pins: pins, IsLock: isLock);
+        });
+
+    public int Star1() => LocksAndKeys.Where(kl => kl.IsLock).Sum(l => LocksAndKeys.Count(k => !k.IsLock && l.Pins.Zip(k.Pins).All(p => p.First + p.Second <= 5)));
+
+    public int Star2()
+    {
+        return 0;
+    }
+}
