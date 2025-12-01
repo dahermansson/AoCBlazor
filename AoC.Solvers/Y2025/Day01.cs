ac3deaea1e4bfa85c67c461ef9d2775fc05414b8
DaHermansson <daniel.hermansson@gmail.com>
2025-12-01 23:07:14 +0100

    fix

--- AoC.Solvers/Y2025/Day01.cs
+++ AoC.Solvers/Y2025/Day01.cs
@@ -10,21 +10,19 @@ public class Day01(string input) : IDay
     public int Star1() => Input.Aggregate((Zeros: 0, Position: 50), (agg, current) =>
             (Zeros: GetReminder(agg.Position + current, 100) is { } position && position == 0
             ? agg.Zeros + 1
-            : agg.Zeros, Position: position))
+            : agg.Zeros,
+            Position: position))
         .Zeros;
 
     public int Star2() => Input.Aggregate((Zeros: 0, Position: 50), (agg, current) =>
-        {
-            return (Zeros: GetReminder(agg.Position + current, 100) is { } position &&
-                position == 0 ||
-                agg.Position != 0 &&
-                current < 0 && position > agg.Position
-                || (current > 0 && position < agg.Position)
-                ? agg.Zeros + 1 + GetLaps(current)
-                : agg.Zeros + GetLaps(current)
-                , Position: position);
+            (Zeros: GetReminder(agg.Position + current, 100) is { } position && position == 0 ||
+            agg.Position != 0 &&
+            current < 0 && position > agg.Position
+            || (current > 0 && position < agg.Position)
+            ? agg.Zeros + 1 + GetLaps(current)
+            : agg.Zeros + GetLaps(current),
+            Position: position)).Zeros;
 
-        }).Zeros;
 
     static int GetLaps(int distance) => (int)Math.Floor(Math.Abs(distance) / (double)100);
     private static int GetReminder(int a, int b) => a - b * (int)Math.Floor(a / (double)b);
