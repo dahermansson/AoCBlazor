1a3bb9ee6621c4f64bcba56d3f9a5291a7a30a7c
DaHermansson <daniel.hermansson@gmail.com>
2025-12-04 21:40:14 +0100

    refactor day 4

--- AoC.Solvers/Y2025/Day04.cs
+++ AoC.Solvers/Y2025/Day04.cs
@@ -6,60 +6,49 @@ public class Day04(string input) : IDay
 
     private string[] Input { get; set; } = InputParsers.GetInputLines(input);
 
-    public int Star1()
+    private static List<Pos> AccessedByAForklift(char[][] grid)
     {
-        var grid = Input.Select(row => row.Select(col => col).ToArray()).ToArray();
         int rows = grid.Length;
         int cols = grid[0].Length;
 
-        var accessable = Enumerable.Range(0, rows).SelectMany(row => {
-            var k =  Enumerable.Range(0, cols).Where(col =>
-            {
-                return grid[row][col] == '@' && Adjacents.Count(a => 
-                    row + a.Row >= 0 && row + a.Row < rows &&
-                    col + a.Col >= 0 && col + a.Col < cols &&
-                    grid[row + a.Row][col + a.Col] == '@') < 4;
-            });
-            return k.Select(a => new Pos(row, a));
-        });
+        IEnumerable<Pos> ValidAdjacents(int currentRow, int currentCol)
+            => Adjacents().Select(a
+                => new Pos(currentRow + a.Row, currentCol + a.Col))
+                    .Where(pos => pos.Row > -1 && pos.Row < rows
+                                && pos.Col > -1 && pos.Col < cols);
 
-        return accessable.Count();
+        return Enumerable.Range(0, rows).SelectMany(
+            row => Enumerable.Range(0, rows)
+                .Where(col => grid[row][col] == '@'
+                    && ValidAdjacents(row, col).Count(adjacent =>
+                        grid[adjacent.Row][adjacent.Col] == '@') < 4
+                ).Select(c => new Pos(row, c))).ToList();
     }
 
+    public int Star1() => 
+        AccessedByAForklift([..Input.Select(row => row.Select(col => col).ToArray())]).Count;
+        
     public int Star2()
     {
-        var grid = Input.Select(row => row.Select(col => col).ToArray()).ToArray();
-        int rows = grid.Length;
-        int cols = grid[0].Length;
-        List<Pos> accessable = [];
-        do {
-            accessable = Enumerable.Range(0, rows).SelectMany(row => {
-            var k =  Enumerable.Range(0, cols).Where(col =>
-            {
-                return grid[row][col] == '@' && Adjacents.Count(a => 
-                    row + a.Row >= 0 && row + a.Row < rows &&
-                    col + a.Col >= 0 && col + a.Col < cols &&
-                    (grid[row + a.Row][col + a.Col] == '@')) < 4;
-            });
-            return k.Select(a => new Pos(row, a));
-            }).ToList();
-            accessable.ForEach(f => grid[f.Row][f.Col] = 'x');
+        char[][] grid = [.. Input.Select(row => row.Select(col => col).ToArray())];
 
-        } while (accessable.Count != 0);
-        
+        while (AccessedByAForklift(grid) is { Count: > 0 } accessedByAForklift)
+            accessedByAForklift.ForEach(f => grid[f.Row][f.Col] = 'x');
+
         return grid.SelectMany(t => t).Count(c => c == 'x');
     }
 
     private record Pos(int Row, int Col);
 
-    private static readonly List<Pos> Adjacents = [
-        new (-1, -1),
-        new (-1, 0),
-        new (-1, 1),
-        new (0, -1),
-        new (0, 1),
-        new (1, -1),
-        new (1, 0),
-        new (1, 1)
-    ]; 
+    private static IEnumerable<Pos> Adjacents()
+    {
+        yield return new(-1, -1);
+        yield return new(-1, 0);
+        yield return new(-1, 1);
+        yield return new(0, -1);
+        yield return new(0, 1);
+        yield return new(1, -1);
+        yield return new(1, 0);
+        yield return new(1, 1);
+    }
 }
