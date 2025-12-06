ac1767598913bf0a9137ca5f47c260502d73c851
DaHermansson <daniel.hermansson@gmail.com>
2025-12-06 21:02:59 +0100

    refactor day2

--- AoC.Solvers/Y2025/Day02.cs
+++ AoC.Solvers/Y2025/Day02.cs
@@ -7,38 +7,38 @@ public class Day02(string input) : IDay
 
     private string[] Input { get; set; } = input.Split(",");
 
+    private IEnumerable<IEnumerable<long>> ProductIds => Input.Select(t =>
+    {
+        var k = t.Split('-').Select(long.Parse).ToArray();
+        return CreateRange(k[0], k[1] - k[0] + 1);
+    });
+
+    private static bool ValidStar1(long productId)
+    {
+        var productIdString = productId.ToString();
+        var l = productIdString.Length / 2;
+        return productIdString[..l] == productIdString[l..];
+    }
+
     public int Star1()
     {
-        var invalid = Input.Select(t => {
-            var k = t.Split('-').Select(long.Parse).ToArray();
-            return CreateRange(k[0], k[1]-k[0] + 1);
-        }).SelectMany(t => t).Where(i => {
-            var t = i.ToString();
-            var l = t.Length/2;
-            return t[..l] == t[l..];
-        }).ToList();
-
-        _output = invalid.Sum().ToString();
+        _output = ProductIds.SelectMany(t => t).Where(ValidStar1).Sum().ToString();
         return -1;
     }
 
-    public int Star2()
-    {        
-        var invalid = Input.Select(t => {
-                var k = t.Split('-').Select(long.Parse).ToArray();
-                return CreateRange(k[0], k[1]-k[0] + 1);
-            }).SelectMany(t => t).Where(i => {
-                var t = i.ToString();
-                var l = t.Length/2;
-            
-                return Enumerable.Range(1, l).Any(c => 
-                {
-                    var chunks = t.Chunk(c).Select(p => string.Concat(p)).ToArray();
-                    return chunks.Skip(1).All(p => p == chunks[0]);
-                });
-            }).ToList();
+    private static bool ValidStar2(long productId)
+    {
+        var productIdString = productId.ToString();
+        return Enumerable.Range(1, productIdString.Length/2).Any(c =>
+        {
+            var chunks = productIdString.Chunk(c).Select(p => string.Concat(p)).ToArray();
+            return chunks.Skip(1).All(p => p == chunks[0]);
+        });
+    }
 
-        _output = invalid.Sum().ToString();
+    public int Star2()
+    {
+        _output = ProductIds.SelectMany(t => t).Where(ValidStar2).Sum().ToString();
         return -1;
     }
 
